19214a0a47662001c5295d5bcec8aa703b72033b
Thomas Grainger <tagrain@gmail.com>
2018-04-24 11:08:48 +0100

    Back to development: 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.0.1'
+version = '0.0.2.dev0'
 
 
 def read_file(filename):
